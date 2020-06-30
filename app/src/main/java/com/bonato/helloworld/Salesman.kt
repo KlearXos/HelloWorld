21da581b7e5157655277efeb16f2e5e46def4710
Filippo Bonato <f.bonato@4p-online.com>
2020-06-30 16:39:53 +0200

    again

--- app/src/main/java/com/bonato/helloworld/Salesman.kt
+++ app/src/main/java/com/bonato/helloworld/Salesman.kt
@@ -1,8 +1,14 @@
 package com.bonato.helloworld
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity;
+import androidx.room.PrimaryKey
 
 @Entity(tableName="salesman_table")
-abstract class Salesman {
+class Salesman {
+
+    @PrimaryKey
+    var id : String,
+    @ColumnInfo var name : String
 
 }
