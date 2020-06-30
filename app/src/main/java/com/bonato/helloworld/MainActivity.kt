f8f089d80b83a36eac0d1d2dab45961ff8d5ade6
Filippo Bonato <f.bonato@4p-online.com>
2020-06-30 16:20:28 +0200

    playing with entity

--- app/src/main/java/com/bonato/helloworld/MainActivity.kt
+++ app/src/main/java/com/bonato/helloworld/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        // the state saved prior during the onpause method often..
+        // it can be null
+        super.onCreate(savedInstanceState) //call on Create in the base class
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/bonato/helloworld/Salesman.kt
@@ -0,0 +1,8 @@
+package com.bonato.helloworld
+
+import androidx.room.Entity;
+
+@Entity(tableName="salesman_table")
+abstract class Salesman {
+
+}
