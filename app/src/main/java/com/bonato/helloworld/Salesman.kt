b3d535e11d520726410bd8674279f49cb18c7ce9
Filippo Bonato <f.bonato@4p-online.com>
2020-07-02 14:10:13 +0200

    exploring primary constructor and data classes

--- app/src/main/java/com/bonato/helloworld/Salesman.kt
+++ app/src/main/java/com/bonato/helloworld/Salesman.kt
@@ -1,14 +1,13 @@
 package com.bonato.helloworld
 
 import androidx.room.ColumnInfo
-import androidx.room.Entity;
+import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Relation
+import androidx.room.Embedded
 
-@Entity(tableName="salesman_table")
-class Salesman {
+@Entity(tableName="head2")
+data class Salesman(@PrimaryKey var id : Long, @ColumnInfo var name : String, @ColumnInfo var depot : Long, @ColumnInfo var dep_name : String) {
 
-    @PrimaryKey
-    var id : String,
-    @ColumnInfo var name : String
-
+    @ColumnInfo var phone : String = ""
 }
