6c60c018830bdddedd8bd273f5c38ae37f940cda
Filippo Bonato <f.bonato@4p-online.com>
2020-07-07 14:06:41 +0200

    adding Dao

---
+++ app/src/main/java/com/bonato/helloworld/CustomerDao.kt
@@ -0,0 +1,34 @@
+package com.bonato.helloworld
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Update
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.OnConflictStrategy
+
+@Dao
+interface CustomerDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(vararg customers: Customer)
+
+    @Insert
+    fun insert(lst : List<Customer>)
+
+    @Update
+    fun update(vararg customers: Customer)
+
+    @Update
+    fun update(lst : List<Customer>)
+
+    @Delete
+    fun delete(vararg customers: Customer)
+
+    @Delete
+    fun delete(lst : List<Customer>)
+
+    @Query("SELECT * FROM 'Ana2*****'")
+    fun loadAll(): Array<Customer>
+
+    @Query("SELECT * FROM 'Ana2*****' WHERE id = :id")
+    fun load(id : Long): Customer
+}
